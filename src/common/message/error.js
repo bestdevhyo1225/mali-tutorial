5327d24c4c41058ff8c6b5de7ac21f1a6887dda7
janghyoseog <bestdevhyo1225@gmail.com>
2020-04-13 16:53:46 +0900

    feat: Add error message and code

---
+++ src/common/message/error.js
@@ -0,0 +1,9 @@
+const E000 = { code: 0, detail: "Deadline ERROR.", status: "CANCELLED" };
+const E001 = { code: 1, detail: "찾을 수 없습니다.", status: "NOT_FOUND" };
+const E002 = { code: 2, detail: "에러가 발생했습니다.", status: "INTERNAL" };
+
+export default {
+  E000,
+  E001,
+  E002
+};
